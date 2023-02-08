f0fd46dea7cb63f93332de8cbe11336a32b433ac
Crowdbotics <engineering@crowdbotics.com>
2023-02-08 06:16:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-dream-dev-62066",
+  url: "https://hidden-dream-dev-62066.botics.co",
+  api: "https://hidden-dream-dev-62066.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
